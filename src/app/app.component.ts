ed8d756ce758c783268e5d30b43dacbcbec7b6ec
26Sho <avilashortyn0@gmail.com>
2026-03-16 16:52:16 -0500

    Actualización de edad periodista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -129,4 +129,22 @@ export class AppComponent implements OnInit {
     this.personInfo = null;
   }
 
+  getAge(birthday: string): number {
+
+  if (!birthday) return 0;
+
+  const birthDate = new Date(birthday);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  const month = today.getMonth() - birthDate.getMonth();
+
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+}
+
 }
